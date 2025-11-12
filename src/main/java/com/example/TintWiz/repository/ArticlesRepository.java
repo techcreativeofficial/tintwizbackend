938a605d2519daccc178f7da217a096136e73789
Yoghi Setiawan <the3verlastin9@gmail.com>
2025-11-12 23:23:34 +0700

    add articles repo

---
+++ src/main/java/com/example/TintWiz/repository/ArticlesRepository.java
@@ -0,0 +1,8 @@
+package com.example.TintWiz.repository;
+
+import com.example.TintWiz.entity.Articles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticlesRepository extends JpaRepository<Articles,Long> {
+
+}

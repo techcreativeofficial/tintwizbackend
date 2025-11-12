c43bfe3f717d685e19afb4afdc156ab62fdabc54
Yoghi Setiawan <the3verlastin9@gmail.com>
2025-11-13 01:14:19 +0700

    Create ProductService

---
+++ src/main/java/com/example/TintWiz/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.example.TintWiz.service;
+
+import com.example.TintWiz.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+}

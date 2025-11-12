3c7c858d98f59282f576b9d8280bb3be42933b56
Yoghi Setiawan <the3verlastin9@gmail.com>
2025-11-12 23:23:43 +0700

    add assets repo

---
+++ src/main/java/com/example/TintWiz/repository/AssetsRepository.java
@@ -0,0 +1,8 @@
+package com.example.TintWiz.repository;
+
+import com.example.TintWiz.entity.Assets;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetsRepository extends JpaRepository<Assets,Long> {
+
+}

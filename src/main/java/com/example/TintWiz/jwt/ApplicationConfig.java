cb2cae9f06926803a05bce111766f3b31ac27401
Yoghi Setiawan <the3verlastin9@gmail.com>
2025-11-14 15:03:41 +0700

    add security configurations

---
+++ src/main/java/com/example/TintWiz/jwt/ApplicationConfig.java
@@ -0,0 +1,57 @@
+package com.example.TintWiz.jwt;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableMethodSecurity
+public class ApplicationConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+//        UserDetails admin = User.withUsername("USER")
+//                .password(passwordEncoder().encode("USER"))
+//                .roles("USER")
+////                .accountExpired(false)
+////                .accountLocked(false)
+////                .credentialsExpired(false)
+////                .disabled(false)
+//                .build();
+//        UserDetails user = User.withUsername("ADMIN")
+//                .password(passwordEncoder().encode("ADMIN"))
+//                .roles("USER","ADMIN","HR")
+////                .accountExpired(false)
+////                .accountLocked(false)
+////                .credentialsExpired(false)
+////                .disabled(false)
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user); //get user,password from UserDetailsService
+        return new CustomUserDetailsService(); // get user,password,email,status,etc from database
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+}

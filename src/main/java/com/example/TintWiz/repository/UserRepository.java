d28e11751ff89ebee64a4d68272746896356d408
Yoghi Setiawan <the3verlastin9@gmail.com>
2025-11-14 16:47:47 +0700

    .

--- src/main/java/com/example/TintWiz/repository/UserRepository.java
+++ src/main/java/com/example/TintWiz/repository/UserRepository.java
@@ -14,6 +14,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
     User findByIduser(Long iduser);
 
     @Query("select u from User u where u.username=:username")
